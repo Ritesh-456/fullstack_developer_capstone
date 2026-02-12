6ae00fc5df7d55ce52c76a81196b1764cfa06adc
Ritesh-456 <20gudu03@gmail.com>
2026-02-12 18:17:20 +0530

    updating contact.html

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://brahmacharir-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://brahmacharir-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.piedpiper.com', '.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://*.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
